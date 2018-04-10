7a2a7e6103f0054eb98dd132fe864177ca51c928
Yvonne.qian <m.qian@foxmail.com>
2018-04-10 16:07:54 +0800

    add modal for help info

---
+++ src/app/my-components/modal/modal.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { TEAMSCORE, PERSONSCORE } from './modal.constant';
+
+@Component({
+  selector: 'ngbd-modal',
+  templateUrl: './modal.component.html'
+})
+export class NgbdModalComponent implements OnInit {
+  @Input() isTeamLeader = false;
+  firScores = [1, 2, 3, 4, 5];
+  secScores = [6, 7, 8, 9, 10];
+  contentObj = [];
+
+  constructor(public activeModal: NgbActiveModal) {}
+
+  ngOnInit() {
+    if (this.isTeamLeader) {
+      this.contentObj = PERSONSCORE;
+      this.secScores = [];
+    } else {
+      this.contentObj = TEAMSCORE;
+    }
+
+  }
+}
