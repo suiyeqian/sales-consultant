936e4276be98d1a4063b3849836e47a4bb9db60e
Yvonne.qian <m.qian@foxmail.com>
2018-04-24 14:53:22 +0800

    add constant

---
+++ src/app/pages/track/track-detail/constant.ts
@@ -0,0 +1,89 @@
+export const NavList = [
+  {
+    name: '合同金额',
+    code: 'htje',
+    datas: [
+      {
+        idxName: '合同金额', type: 'bar', unit: '万元', value: 0
+      }
+    ]
+  }, {
+    name: '申请单量',
+    code: 'sqdl',
+    datas: [
+      {
+        idxName: '申请单量', type: 'bar', unit: '笔', value: 0
+      }
+    ]
+  }, {
+    name: '通过单量',
+    code: 'tgdl',
+    datas: [
+      {
+        idxName: '通过单量', type: 'bar', unit: '笔', value: 0
+      }
+    ]
+  }, {
+    name: '拒绝单量',
+    code: 'jjdl',
+    datas: [
+      {
+        idxName: '拒绝单量', type: 'bar', unit: '笔', value: 0
+      }
+    ]
+  }, {
+    name: '放款单量',
+    code: 'fkdl',
+    datas: [
+      {
+        idxName: '放款单量', type: 'bar', unit: '笔', value: 0
+      }
+    ]
+  }, {
+    name: '通过率',
+    code: 'tgl',
+    datas: [
+      {
+        idxName: '通过率', type: 'bar', unit: '%', value: 0
+      }
+    ]
+  }, {
+    name: '合同件均',
+    code: 'htjj',
+    datas: [
+      {
+        idxName: '合同件均', type: 'bar', unit: '万元', value: 0
+      }
+    ]
+  }, {
+    name: '批核情况',
+    code: 'phqk',
+    datas: [
+      {
+        idxName: '批核金额', type: 'bar', unit: '万元', value: 0
+      }, {
+        idxName: '批核件均', type: 'line', unit: '万元', value: 0
+      }
+    ]
+  }, {
+    name: '签约情况',
+    code: 'qyqk',
+    datas: [
+      {
+        idxName: '签约金额', type: 'bar', unit: '万元', value: 0
+      }, {
+        idxName: '签约件数', type: 'line', unit: '件', value: 0
+      }
+    ]
+  }, {
+    name: '待签情况',
+    code: 'dqqk',
+    datas: [
+      {
+        idxName: '待签金额', type: 'bar', unit: '万元', value: 0
+      }, {
+        idxName: '待签件数', type: 'line', unit: '件', value: 0
+      }
+    ]
+  },
+];
